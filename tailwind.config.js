f02a0b43ee8b9fdb4ba12d23dcc61fb62b35afc2
destinybravos <destinybravos@gmail.com>
2024-11-19 15:42:10 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'selector',
+  content: ["./app/**/*.{html,css,php,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
