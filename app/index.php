80f0f0e2aa12deead2828943f19cb8ca8491a99d
destinybravos <destinybravos@gmail.com>
2024-11-22 13:44:12 +0100

    Added Website Structure and Components

---
+++ app/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Market</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="./assets/fa-icons/css/all.css">
+</head>
+<body class="">
+    <?php 
+        include_once './components/site_header.html';
+        include_once './components/hero_section.html';
+        include_once './components/service_grid.html';
+    ?>
+</body>
+</html>
