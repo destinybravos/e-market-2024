d9fb682412850de9e242dad5373d18a75a20323f
destinybravos <destinybravos@gmail.com>
2024-11-28 17:21:12 +0100

    Testing and API

---
+++ app/products.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Market</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="./assets/fa-icons/css/all.css">
+</head>
+<body class="">
+    <?php 
+        include_once './components/site_header.html';
+        include_once './components/products/all.html';
+        include_once './components/products/latest.html';
+        include_once './components/products/featured.html';
+        include_once './components/products/trending.html';
+    ?>
+</body>
+</html>
