fc796fc1e6e4990ee2e389e29ca378e61c5ab3a0
destinybravos <destinybravos@gmail.com>
2024-12-05 17:00:04 +0100

    Did product details and update card design

---
+++ api/products/product_list.php
@@ -0,0 +1,61 @@
+<?php
+
+$products = [
+    [
+        'id' => '1',
+        'category' => 'Phone gadgets',
+        'name' => 'samsung Iwatch 12gen.',
+        'price' => 230000,
+        'description' => '',
+        'rating' => 4,
+        'no_reviews' => 90,
+        'discount' => 30,
+        'image' => 'http://localhost/e-market-2024/app/images/gad1.avif'
+    ],
+    [
+        'id' => '2',
+        'category' => 'Beauty & Cosmetics',
+        'name' => 'NIVEA Dry Comfort Roll-on For Women, 72h- 50ml (Pack Of 3)',
+        'price' => 3500,
+        'description' => '',
+        'rating' => 5,
+        'no_reviews' => 5,
+        'discount' => 0,
+        'image' => 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/64/9523401/1.jpg?5543'
+    ],
+    [
+        'id' => '3',
+        'category' => 'Beauty & Cosmetics',
+        'name' => 'Foreign Trade Piano Color Balayage Highlight Straight Bob Wig 10A 4x4 BOB 150 180 Density',
+        'price' => 23000,
+        'description' => '',
+        'rating' => 3.5,
+        'no_reviews' => 27,
+        'discount' => 25,
+        'image' => 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/01/8589733/1.jpg?0860'
+    ],
+    [
+        'id' => '4',
+        'category' => 'Home Appliances',
+        'name' => 'Home Rugs Living Bedroom Plush Rugs Grass Green',
+        'price' => 22000,
+        'description' => '',
+        'rating' => 1.4,
+        'no_reviews' => 100,
+        'discount' => 0,
+        'image' => 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/29/5857543/1.jpg?4692'
+    ],
+    [
+        'id' => '5',
+        'category' => 'Phone gadgets',
+        'name' => 'Protective Cover For Airpods Universal Headset Case Wireless Earphone Silicone Case',
+        'price' => 13743,
+        'description' => '',
+        'rating' => 4,
+        'no_reviews' => 34,
+        'discount' => 0,
+        'image' => 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/16/4812733/1.jpg?1146'
+    ]
+];
+
+?>
---
+++ app/product_details.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Market</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="./assets/fa-icons/css/all.css">
+</head>
+
+<body class="">
+    <?php
+    include_once './components/site_header.html';
+    ?>
+
+    <section class="my-3 px-6">
+        <a href="products.php">
+            <i class="fas fa-arrow-left"></i> Back
+        </a>
+    </section>
+
+    <section class="max-w-6xl mx-auto py-16">
+        <div class="flex flex-col md:flex-row items-start gap-8" id="product_details">
+
+        </div>
+    </section>
+
+
+    <script>
+        let baseURL = 'http://localhost/e-market-2024/api';
+
+        let noOfStars = (rating) => {
+            let starRating = Math.round(rating);
+            let stars = '';
+
+            for (let i = 0; i < starRating; i++) {
+                stars += '<i class=" text-yellow-300 fa-solid fa-star"></i>';
+            }
+
+            for (let i = 0; i < (5 - starRating); i++) {
+                stars += '<i class=" text-slate-300 fa-solid fa-star"></i>';
+            }
+
+
+            return stars;
+        }
+
+        let url = new URL(location.href) // create a new URL object
+        let productId = url.searchParams.get('product_id'); // get the product id from the url parameter into the productId variable
+
+        // Make an API request to fetch the details of the product with the productId
+        fetch(`${baseURL}/products/single_product.php?id=${productId}`)
+            .then((res) => res.json())
+            .then((product) => {
+                let details = `<div class="flex-1 w-1/2 overflow-hidden">
+                                <img src="${product.image}" alt="Image" class="object-cover">
+                            </div>
+
+                            <div class="flex-1 w-1/2 py-6">
+                                <h1 class="text-lg mb-3">${product.category}</h1>
+                                <h1 class="text-3xl font-bold">${product.name}</h1>
+                                <p>
+                                    ${ noOfStars(product.rating) }
+                                </p>
+                                <hr class="my-6">
+                                <div class="text-xl font-bold">
+                                     &#8358; ${product.price - (product.price * (product.discount/100))}
+                                </div>
+                                <div class="text-sm line-through text-slate-400">
+                                    ${ (product.discount > 0) ? `<small class="line-through text-sm text-slate-500">
+                                        &#8358; ${product.price}
+                                    </small>` : '' }
+                                </div>
+                            </div>`;
+
+                document.getElementById('product_details').innerHTML = details;
+            })
+    </script>
+</body>
+
+</html>
