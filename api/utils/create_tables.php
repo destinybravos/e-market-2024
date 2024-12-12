67f356cf66e93a1ba317944d11b49e915661fa31
destinybravos <destinybravos@gmail.com>
2024-12-12 17:33:54 +0100

    Added Database Migration files and Created Admin Structure

---
+++ api/utils/create_tables.php
@@ -0,0 +1,58 @@
+<?php
+
+    include_once "connect.php";
+
+    // Create User Table
+    $sqlStatement = "CREATE TABLE IF NOT EXISTS users(
+        id INT(6) PRIMARY KEY AUTO_INCREMENT,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30),
+        email VARCHAR(50) NOT NULL UNIQUE,
+        password VARCHAR(100) NOT NULL,
+        created_at DATETIME DEFAULT(CURRENT_TIMESTAMP)
+    )";
+    $conn->query($sqlStatement);
+
+    // Create Shop Tables
+    $sqlStatement = "CREATE TABLE IF NOT EXISTS shops(
+        id INT(6) PRIMARY KEY AUTO_INCREMENT,
+        user_id INT(6) NOT NULL,
+        shop_name VARCHAR(30) NOT NULL,
+        address VARCHAR(50),
+        city VARCHAR(50),
+        state VARCHAR(50),
+        phone VARCHAR(50) UNIQUE,
+        description TEXT,
+        created_at DATETIME DEFAULT(CURRENT_TIMESTAMP),
+        CONSTRAINT shop_owner FOREIGN KEY (user_id) REFERENCES users(id)
+    )";
+    $conn->query($sqlStatement);
+
+    // Create Shop Tables
+    $sqlStatement = "CREATE TABLE IF NOT EXISTS categories(
+        id INT(6) PRIMARY KEY AUTO_INCREMENT,
+        name INT(6) NOT NULL,
+        description TEXT
+    )";
+    $conn->query($sqlStatement);
+
+    // Create Shop Tables
+    $sqlStatement = "CREATE TABLE IF NOT EXISTS products(
+        id INT(6) PRIMARY KEY AUTO_INCREMENT,
+        category_id INT(6) NOT NULL,
+        shop_id INT(6) NOT NULL,
+        name VARCHAR(100) NOT NULL,
+        price DOUBLE(10, 2),
+        description MEDIUMTEXT,
+        discount INT(4),
+        image VARCHAR(1000),
+        created_at DATETIME DEFAULT(CURRENT_TIMESTAMP),
+        CONSTRAINT product_category FOREIGN KEY (category_id) REFERENCES categories(id),
+        CONSTRAINT product_shop FOREIGN KEY (shop_id) REFERENCES shops(id)
+    )";
+    $conn->query($sqlStatement);
+
+
+    echo "All table created successfully <br>";
+
+?>
